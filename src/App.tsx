648fb86bb914dcbd0d141f51b38e510ddcf6c06c
Abdul Moiz <moizkhalid330@gmail.com>
2025-08-25 16:36:23 +0500

    merged both old code and new code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import Alert from "./components/Alert";
+import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
       <Alert>
         <span>Yoo Alert | Woo hooo</span>
       </Alert>
